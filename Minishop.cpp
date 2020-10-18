14e9daf9106af7fda802791f3769a0812dbc3609
Aaronarch5 <63936853+Aaronarch5@users.noreply.github.com>
2020-10-18 17:56:43 -0500

    Clocale

--- Minishop.cpp
+++ Minishop.cpp
@@ -6,24 +6,24 @@
 using namespace std;
 int main()
 {
- 
+    setlocale(LC_ALL,"");
     cout<<"Mini punto de venta Guerrero-Hernandez"<<endl;
-    
+
     float producto1{2.98};
     float producto2{4.5};
     float producto3{9.98};
     float producto4{4.49};
     float producto5{6.87};
-    
+
     int numeroproducto{};
     int cantidadvendida{};
-    
-    
+
+
     cout<<"Introduzca que producto quiere comprar: "<<endl;
     cin>>numeroproducto;
     cout<<"Introduzca cuantas piezas quiere comprar"<<endl;
     cin>>cantidadvendida;
-    
-    
+
+
 }
 
