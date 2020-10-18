6f03ed90e70df0ca2a7b745bc3865c84f08c736e
Aaronarch5 <63936853+Aaronarch5@users.noreply.github.com>
2020-10-18 17:48:44 -0500

    Ajustes iniciales

--- Minishop.cpp
+++ Minishop.cpp
@@ -5,12 +5,23 @@
 
 int main()
 {
-    float producto1{};
-    float producto2{};
-    float producto3{};
-    float producto4{};
+    cout<<"Mini punto de venta Guerrero-Hernandez"<<endl;
     
-    cout<<"C++ Proyecto"<<endl;
-    cout<<"Proyecto"<<endl;
+    float producto1{2.98};
+    float producto2{4.5};
+    float producto3{9.98};
+    float producto4{4.49};
+    float producto5{6.87};
+    
+    int numeroproducto{};
+    int cantidadvendida{};
+    
+    
+    cout<<"Introduzca que producto quiere comprar: "<<endl;
+    cin<<numeroproducto;
+    cout<<"Introduzca cuantas piezas quiere comprar"<<endl;
+    cin<<cantidadvendida;
+    
+    
 }
 
