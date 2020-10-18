485894d475b380f68d5b9516d2d9d8eeb621fe0c
Aaronarch5 <63936853+Aaronarch5@users.noreply.github.com>
2020-10-18 18:03:37 -0500

    .

--- Minishop.cpp
+++ Minishop.cpp
@@ -1,6 +1,9 @@
 // Minishop.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 // Programación Orientada a Objetos
+/*
 
+@
+*/
 #include <iostream>
 #include <clocale>
 using namespace std;
