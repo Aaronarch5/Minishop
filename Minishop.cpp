87d7e446a0e3d04585f71b18485d110c7a7743d1
Cesar12103 <72959728+Cesar12103@users.noreply.github.com>
2020-10-18 23:00:07 -0500

    Codigo elemental
    Ya está el código con lo que pide el maestro, calenlo

--- Minishop.cpp
+++ Minishop.cpp
@@ -6,6 +6,7 @@
 */
 #include <iostream>
 #include <clocale>
+#include <cctype>
 using namespace std;
 int main()
 {
@@ -13,20 +14,72 @@ int main()
     cout<<"Mini punto de venta Guerrero-Hernandez"<<endl;
 
     float producto1{2.98};
-    float producto2{4.5};
+    float producto2{4.50};
     float producto3{9.98};
     float producto4{4.49};
     float producto5{6.87};
+    float total{0};
+    float subsuma1{0};
+    float subsuma2{0};
+    float subsuma3{0};
+    float subsuma4{0};
+    float subsuma5{0};
 
-    int numeroproducto{};
-    int cantidadvendida{};
+    int numeroproducto{0};
+    int cantidadvendida{0};
+    
+    char respuesta{};
 
-
-    cout<<"Introduzca que producto quiere comprar: "<<endl;
+	///////////////////////////////////////////////////////////	
+	cout<<"Listado de productos: "<<endl;
+	cout<<"Producto 1: "<<producto1<<endl;
+	cout<<"Producto 2: "<<producto2<<endl;
+	cout<<"Producto 3: "<<producto3<<endl;
+	cout<<"Producto 4: "<<producto4<<endl;
+	cout<<"Producto 5: "<<producto5<<endl;
+    ///////////////////////////////////////////////////////////
+do
+{
+	cout<<"**Introduzca que producto quiere comprar: "<<endl;
     cin>>numeroproducto;
-    cout<<"Introduzca cuantas piezas quiere comprar"<<endl;
+    cout<<"**Introduzca cuantas piezas quiere comprar:"<<endl;
     cin>>cantidadvendida;
-
-
+    
+	switch (numeroproducto)
+	{
+		case 1:
+			subsuma1=producto1*cantidadvendida;
+			break;
+		
+		case 2:
+			subsuma2=producto2*cantidadvendida;
+			break;
+			
+		case 3:
+			subsuma3=producto3*cantidadvendida;
+			break;
+			
+		case 4:
+			subsuma4=producto4*cantidadvendida;
+			break;
+			
+		case 5:
+			subsuma5=producto5*cantidadvendida;
+			break;
+	}
+	
+	cout<<"¿Desea introducir otra producto? (s/n)"<<endl;
+	cin>>respuesta;
+	
+}
+	while (tolower(respuesta)=='s');
+	cout<<"Fin de la compra"<<endl;
+	
+	
+	cout<<"_________________________________________"<<endl;
+	cout<<" "<<endl;
+	
+	total=subsuma1+subsuma2+subsuma3+subsuma4+subsuma5;
+	cout<<"El total es: "<<total<<endl;
 }
 
